042139f725acdbf45dafff4fc3cb867da1127b67
Ram Warhekar <151377884+ramwarhekar02@users.noreply.github.com>
2025-04-10 16:42:18 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://univaries-ecom-web-frontend.vercel.app/",
     credentials: true,
 }))
 
